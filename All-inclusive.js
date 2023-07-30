a3f2a540d9a6b3b77e6e4cb834234fa668d46efb
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-29 22:46:57 -0700

    Update All-inclusive.js

--- All-inclusive.js
+++ All-inclusive.js
@@ -20,3 +20,12 @@ Note:
 
 
 // Solution
+
+function containAllRots(str, arr) {
+  for (var i = 0; i < str.length; i++) {
+    if (arr.indexOf(str.slice(i) + str.slice(0, i)) === -1) {
+      return false
+    }
+  }
+  return true
+}
